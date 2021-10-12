cd38cf328be391293d43779e793f51bd97eea807
asadullahjamalov <asadullahnj@code.edu.az>
2021-10-12 22:58:49 +0400

    create project

---
+++ src/main/java/com/company/calendarmanagementsystem/CalendarManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.calendarmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/calendarmanagementsystem/CalendarManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.calendarmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
